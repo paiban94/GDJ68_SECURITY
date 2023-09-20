557f190bd56cdc68b0f2a10e374b1127a7d7767a
Daeki <kimdaeki@gmail.com>
2023-09-20 17:43:18 +0900

    1

--- src/main/java/com/winter/app/Gdj68BootApplication.java
+++ src/main/java/com/winter/app/Gdj68BootApplication.java
@@ -2,8 +2,10 @@ package com.winter.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+//@EnableAspectJAutoProxy
 public class Gdj68BootApplication {
 
 	public static void main(String[] args) {
