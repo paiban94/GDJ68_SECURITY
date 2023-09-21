1e3e9ae0b49b7939e272d24fcc4a985d4aa7e115
Daeki <kimdaeki@gmail.com>
2023-09-21 14:15:33 +0900

    6

--- src/main/java/com/winter/app/config/FileMappingConfig.java
+++ src/main/java/com/winter/app/config/FileMappingConfig.java
@@ -2,6 +2,7 @@ package com.winter.app.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +18,8 @@ public class FileMappingConfig implements WebMvcConfigurer {
 	//요청 URL 경로
 	@Value("${app.url.path}")
 	private String urlPath;
+	
+	
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
