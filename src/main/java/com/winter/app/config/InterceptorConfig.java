82036f9615d84b5cc67f628f3283a6601f871843
Daeki <kimdaeki@gmail.com>
2023-09-21 16:10:08 +0900

    7

--- src/main/java/com/winter/app/config/InterceptorConfig.java
+++ src/main/java/com/winter/app/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.winter.app.interceptors.TestInterceptor;
 
@@ -13,11 +14,18 @@ public class InterceptorConfig implements WebMvcConfigurer{
 	@Autowired
 	private TestInterceptor testInterceptor;
 	
+	@Autowired
+	private LocaleChangeInterceptor localeChangeInterceptor;
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(testInterceptor)
 				.addPathPatterns("/notice/list")
 				
+				;
+		
+		registry.addInterceptor(localeChangeInterceptor)
+				.addPathPatterns("/**")
 				;
 		
 	}
